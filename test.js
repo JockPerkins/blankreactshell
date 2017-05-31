e714815fe9909b9e268418957466c9103c663c03
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-31 14:54:50 +0100

    * various changes and movement

--- test.js
+++ test.js
@@ -49,25 +49,6 @@ function testDatabase(){
       });
   });
 }
-// Gets all files in the target directory, including subdirectories
-function getAllFiles(dir, filelist) {
-  var fs = fs || require('fs'), files = fs.readdirSync(dir);
-  filelist = filelist || [];
-  files.forEach(function(file) {
-    // looks in subdirectories for files
-    if (fs.statSync(dir + file).isDirectory()) {
-      filelist = getAllFiles(dir + file + '/', filelist);
-    }
-    else {
-      // doesn't add to list if its a hidden file
-      if (file.charAt(0) != '.' || file == null) {
-        //filelist.push(file);
-        runJsLinter(dir, file);
-      }
-    }
-  });
-  return filelist;
-};
 // function to run each specified file through js linter
 function runJsLinter(fileDir, fileName){
   var command = "./node_modules/.bin/eslint " + fileDir + fileName;
@@ -82,33 +63,79 @@ function runJsLinter(fileDir, fileName){
     console.log(`stderr: ${stderr}`);
   });
 }
-
+// function to display messages
+function displayMessage(type, place){
+  if(type == 'error'){
+    console.error(chalk.red('Error when testing: ' + place));
+  }
+  else if(type == 'success'){
+    console.log(chalk.green('Success when testing: ' + place));
+  }
+}
+// overall function to run the test
 function runTest(){
   // Run the npm install
   npmInstall().then(() => {
-    console.log(chalk.green('NPM install was successful.'));
+    displayMessage('success', 'npm');
     // Run the database check
     testDatabase().then(() => {
-      console.log(chalk.green('Database connection was successful.'));
-      // Lint the files
-      getAllFiles().then(() => {
-        console.log(chalk.green('Files have all been correctly linted.'));
-        process.exit(0);
-      }).catch((err) => {
-        // Linter has failed
-        console.log(chalk.red('Files need attention, please make the amends listed in the console.'));
-        process.exit(1);
-      });
+      displayMessage('success', 'database');
+      process.exit(0);
     }).catch((err) => {
       // Database has failed
-      //console.error(chalk.red('Database connection was unsuccessful.'));
-      //process.exit(1);
+      displayMessage('error', 'database');
+      process.exit(1);
     });
   }).catch((err) => {
     // NPM install has failed
-    console.error(chalk.red('NPM install was unsuccessful.'));
+    displayMessage('error', 'npm');
     process.exit(1);
   });
 }
 
-runTest();
+
+// Gets all files in the target directory, including subdirectories
+function getAllFiles(dir, filelist) {
+  return new Promise(function (fulfill, reject){
+    var fs = fs || require('fs'), files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      // looks in subdirectories for files
+      if (fs.statSync(dir + file).isDirectory()) {
+        filelist = getAllFiles(dir + file + '/', filelist);
+      }
+      else {
+        // doesn't add to list if its a hidden file
+        if (file.charAt(0) != '.' || file == null) {
+          filelist.push(file);
+          console.log(file);
+          //runJsLinter(dir, file);
+        }
+      }
+    });
+    console.log(filelist);
+    fulfill(filelist);
+  });
+};
+
+function testFiles(){
+  // Lint the files
+  getAllFiles(fileDir).then((result) => {
+    displayMessage('success', 'jslint');
+    process.exit(0);
+  }).catch((err) => {
+    console.log(err);
+    // Linter has failed
+    displayMessage('error', 'jslint');
+    process.exit(1);
+  });
+}
+
+var testing = 1;
+
+if(testing === 1){
+  testFiles();
+}
+else {
+  runTest();
+}
