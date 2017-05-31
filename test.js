77d675239aaed3d0645bdeb1b384760deb0d3bff
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-31 16:46:11 +0100

    * moved a bunch of stuff to different functions... this file needs tidying

--- test.js
+++ test.js
@@ -49,20 +49,6 @@ function testDatabase(){
       });
   });
 }
-// function to run each specified file through js linter
-function runJsLinter(fileDir, fileName){
-  var command = "./node_modules/.bin/eslint " + fileDir + fileName;
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      //console.error(`exec error: ${error}`);
-      console.error(`Test failed with the follow error:`);
-      console.error(`${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
-}
 // function to display messages
 function displayMessage(type, place){
   if(type == 'error'){
@@ -72,6 +58,45 @@ function displayMessage(type, place){
     console.log(chalk.green('Success when testing: ' + place));
   }
 }
+// Gets all files in the target directory, including subdirectories
+function getAllFiles(dir, filelist) {
+  return new Promise(function (fulfill, reject){
+    var fs = fs || require('fs'), files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      if (fs.statSync(dir + file).isDirectory()) {
+        getAllFiles(dir + file + '/', filelist);
+      }
+      else {
+        if (file.charAt(0) != '.') {
+          filelist.push(dir + file);
+        }
+      }
+    });
+    fulfill(filelist);
+  });
+};
+// function to run each specified file through js linter
+function runJsLinter(fileDir, files){
+  return new Promise(function (fulfill, reject){
+    for(var index = 0; index < files.length; index++){
+      var command = "./node_modules/.bin/eslint " + fileDir + files[index];
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          //console.error(`exec error: ${error}`);
+          console.error(`Test failed with the follow error:`);
+          console.error(`${error}`);
+          reject(error);
+        }
+        else {
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+          fulfill();
+        }
+      });
+    }
+  });
+}
 // overall function to run the test
 function runTest(){
   // Run the npm install
@@ -80,43 +105,37 @@ function runTest(){
     // Run the database check
     testDatabase().then(() => {
       displayMessage('success', 'database');
-      process.exit(0);
+      getAllFiles(fileDir).then((result) => {
+        displayMessage('success', 'get files');
+        runJsLinter(fileDir, result).then(() => {
+          displayMessage('success', 'jslint');
+          process.exit(0);
+        }).catch((err) => {
+          displayMessage('error', 'jslint');
+          console.log(err);
+          process.exit(1);
+        })
+      }).catch((err) => {
+        displayMessage('error', 'get files');
+        console.log(err);
+        process.exit(1);
+      });
     }).catch((err) => {
       // Database has failed
       displayMessage('error', 'database');
+      console.log(err);
       process.exit(1);
     });
   }).catch((err) => {
     // NPM install has failed
     displayMessage('error', 'npm');
+    console.log(err);
     process.exit(1);
   });
 }
 
 
-// Gets all files in the target directory, including subdirectories
-function getAllFiles(dir, filelist) {
-  return new Promise(function (fulfill, reject){
-    var fs = fs || require('fs'), files = fs.readdirSync(dir);
-    filelist = filelist || [];
-    files.forEach(function(file) {
-      // looks in subdirectories for files
-      if (fs.statSync(dir + file).isDirectory()) {
-        filelist = getAllFiles(dir + file + '/', filelist);
-      }
-      else {
-        // doesn't add to list if its a hidden file
-        if (file.charAt(0) != '.' || file == null) {
-          filelist.push(file);
-          console.log(file);
-          //runJsLinter(dir, file);
-        }
-      }
-    });
-    console.log(filelist);
-    fulfill(filelist);
-  });
-};
+
 
 function testFiles(){
   // Lint the files
@@ -131,10 +150,14 @@ function testFiles(){
   });
 }
 
-var testing = 1;
+
+var testing = 4;
 
 if(testing === 1){
   testFiles();
+}
+else if(testing == 2){
+
 }
 else {
   runTest();
